dc6e0976e9058915f0e093b6288c64e1da18f3ea
nlindeke <NiklasLindeke@Niklass-MacBook-Air.local>
2016-09-09 10:22:16 +0200

    Comments and basis
    Added general comments and basis function and function for finding hot
    interval

--- DeBoorSplines/CSplines.py
+++ DeBoorSplines/CSplines.py
@@ -1,6 +1,22 @@
-#Project 1 for Numerical Algorithms
+# Project 1 for Numerical Algorithms
+import numpy as np
+import scipy as sp
 
 class CSplines:
+    """
+        Implementation of the De Boor Algorithm for Cubic Splines.
+        
+        
+        Some general information about the code so that Claus will be pleased 
+        with the amount of documentation.
+        
+        input:
+            nodepoints or u_vector / knot_vector
+            
+        output:
+            s(u) (or something)
+            
+    """
     
     def __init__(self,nodepoints):
         self.nodepoints = nodepoints
@@ -8,12 +24,17 @@ class CSplines:
     def __call__(self):
         return # something
         
-    def otherfunction(self):
+    def basisfunc(self, knots):
         return # something
+        
+    def findhot(self, a):
+        a = np.array([1,2,3,4])
+        u = 3.
+        i = (a > u).argmax()
+        return i
         
     #Git commit test
     def countAllTheNumbers(self):
         a=0        
         while True:
            a=a+1 
-    # Exactly
