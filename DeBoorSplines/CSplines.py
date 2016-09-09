9c8d6bb3e808f5b824758794a8d40f650fdf6261
nlindeke <NiklasLindeke@Niklass-MacBook-Air.local>
2016-09-09 08:35:05 +0200

    First Commit

---
+++ DeBoorSplines/CSplines.py
@@ -0,0 +1,12 @@
+#Project 1 for Numerical Algorithms
+
+class CSplines:
+    
+    def __init__(self,nodepoints):
+        self.nodepoints = nodepoints
+    
+    def __call__(self):
+        return # something
+        
+    def otherfunction(self):
+        return # something
