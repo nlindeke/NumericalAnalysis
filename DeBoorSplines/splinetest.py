46b34c72be48d779561f4805aa0157dfd4e66d0d
nlindeke <NiklasLindeke@Niklass-MacBook-Air.local>
2016-09-17 17:40:55 +0200

    Derivatives Test

--- DeBoorSplines/splinetest.py
+++ DeBoorSplines/splinetest.py
@@ -5,6 +5,10 @@ from numpy import *
 """
 
 class dtest:
+    """
+        Test class devised to investigate the relationship of the first and second derivative
+        between two adjacent points
+    """
     def __init__(self):
         self.y = CS.CSplines(CS.spline()[1],CS.spline()[0])()[1]
         self.x = CS.CSplines(CS.spline()[1],CS.spline()[0])()[0]        
@@ -12,20 +16,41 @@ class dtest:
     def deriv(self):
         if len(self.x) == len(self.y):
             for i in range(0,len(self.x)-8,8):
+                # Assuming Polyfit Returns  the vector containing [Ax^3 + Bx^2 + Cx + D] as [ A B C D]
                 left = polyfit(self.x[i:i+4],self.y[i:i+4],3)
                 right = polyfit(self.x[i+4:i+8],self.y[i+4:i+8],3)
+                
+                first_left = left * [3,2,1,0]
+                first_left = first_left[0]**2
+
+                first_right = right * [3,2,1,0]
+                first_right = first_right[0]**2
+                
+                second_left = first_left * [2,1,0,0] 
+                second_right = first_right * [2,1,0,0]
+                
+                if second_right == second_left:
+                    seconds =+ 1
+                    print("Second hooray")
+                    
+                if first_right == first_left:
+                    firsts =+ 1
+                    print("Hooray")
+                    
+            return firsts, seconds
+                
   
         else:
             return "x and y coordinates don't match"
-        return False
 
 class speed:
+    """
+        tests the overall speed of __call__, 
+        I think would be the easiest to use 
+        when testing different versions of functions
+    """
     def testAlgoSpeed(self):
-        """
-            tests the overall speed of __call__, 
-            I think would be the easiest to use 
-            when testing different versions of functions
-        """
+
         a=array([])
         c=CS.CSplines(CS.spline()[1],CS.spline()[0])
         for i in range(30):
