3cba08551ace7f5a5bec1e7e221d27f4f3f8f4d5
nlindeke <NiklasLindeke@Niklass-MacBook-Air.local>
2016-09-22 11:59:07 +0200

    QuasiNewton added

---
+++ QuasiNewton/opt.py
@@ -0,0 +1,27 @@
+from numpy import *
+from scipy import *
+
+class OPC:
+    """
+    Some stuff
+    """
+    
+    def __init__(self, obj_func, grad=None):
+        self.obj_func = obj_func
+        self.grad = grad
+    
+    class QN(OPC):
+        def __call__(self):
+            return False
+    
+    
+    class LS(OPC):
+        class E:
+            def __call__(self):
+                return False
+            
+        class IE:
+            def __call(self):
+                return False
+        
+    
