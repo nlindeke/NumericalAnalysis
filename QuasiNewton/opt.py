21d93f62f04a14fd5907ad6071cdd3af3382b78a
Canneberge <charles.rohart@gmail.com>
2016-09-26 16:47:31 +0200

    Abcd

--- QuasiNewton/opt.py
+++ QuasiNewton/opt.py
@@ -5,10 +5,20 @@ class OPC:
     """
     Some stuff
     """
-    
     def __init__(self, obj_func, grad=None):
         self.obj_func = obj_func
         self.grad = grad
+    def base_newton(self,xzero):
+        x=xzero
+        k=0
+        while termination_criteron!=True:
+            x=x-NewtonDirection(x)
+            k=k+1
+    def NewtonDirection(x):
+        return InvHessian(x)*Gradient(x)
+    def InvHessian(x):
+    def Gradient(x):
+            
     
 class QN(OPC):
     def __call__(self):
