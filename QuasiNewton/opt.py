b94e13a316d098f2143253a982caf845f0432f29
Canneberge <charles.rohart@gmail.com>
2016-09-27 20:08:34 +0200

    We came from the bottom now we here
    Everyday everyday it's alright

--- QuasiNewton/opt.py
+++ QuasiNewton/opt.py
@@ -9,56 +9,35 @@ def f2(x):
 def f3(x):
     return 5*x[0]**3-10*x[1]**2
 
-def grad(f,x):
-    h=10**(-8)
-    dim=len(x)
-    e=identity(dim)
-    arr=zeros((1,dim))
-    for i in range(dim):
-        arr[0][i]=(f(x+h*e[:][i])-f(x))/h
-    return arr
-def besthessian(f,x):
-    h=10**(-8)
-    dim=len(x)
-    e=identity(dim)
-    arr=empty((dim,dim))
-    for i in range(dim):
-        arr[i][:]=array(((grad(f,x+h*e[:][i])-grad(f,x))/h))
-    return arr
     
 class OPC:
     """
     Some stuff
     """
-    def __init__(self, obj_func, grad=None):
+    def __init__(self, obj_func, gradis=None):
         self.obj_func = obj_func
-        self.grad = grad
+        self.gradis = gradis
+        
     def base_newton(self,xzero):
         x=xzero
         termination_criterion=False
         while termination_criterion!=True:
             x=x-self.NewtonDirection(x)
-            if x<=0.0001 and x>=-0.0001:
+            if x.any()<=0.0001 and x.any()>=-0.0001:
                 termination_criterion=True
         return x
+    
     def NewtonDirection(self,x):
-        return self.InvHessian(x)*self.Gradient(x)
+        return transpose(matrix(self.InvHessian(x))*matrix(transpose(self.Gradient(x))))
         
     def InvHessian(self,x):
-        return linalg.inv(hessian())
-
-    def betterhessian(self,grad):
-        dim=shape(grad)[0]
-        matrice=zeros(dim,dim)
-        for i in range(dim):
-            for j in range(dim):
-                matrice[i][j]=None
-        
+        return linalg.inv(self.besthessian(x))
+       
     def Gradient(self,x):
-        if self.grad!=None:
-            return self.grad
+        if self.gradis!=None:
+            return self.gradis
         else:
-            return gradient(x)
+            return self.grad(x)
             
     def dimgrid(self,dim,nbvalues=1000,step=1):
         A=zeros((nbvalues,dim))
@@ -74,22 +53,26 @@ class OPC:
             for j in range(nbvalues):
                 matricevaleurs[i][j]=f(i*step,j*step)
         return matricevaleurs
-    def computefuncneu(self,dim,nbvalues=1000,step=1):
-        #We decide the dimension to be 2 for the time being
+    
+    def grad(self,x):
         f=self.obj_func
-        matricevaleurs=zeros((nbvalues,nbvalues))
-        for i in itertools.product(range(nbvalues),dim):
-                matricevaleurs[i][j]=f(i*step,j*step)
-        return matricevaleurs
-    
-    def gradientmaison(self,dim,point,nbvalues):
-        matricefinale=zeros((nbvalues,nbvalues))
-        matricevaleurs=computefunc(self.obj_func,10)
-        for i in range(nbvalues):
-            for j in range(nbvalues):
-                matricefinale[i][j]=None
-        None
-    
+        h=10**(-8)
+        dim=len(x)
+        e=identity(dim)
+        arr=zeros((1,dim))
+        for i in range(dim):
+            arr[0][i]=(f(x+h*e[:][i])-f(x))/h
+        return arr
+    def besthessian(self,x):
+        f=self.obj_func
+        h=10**(-8)
+        dim=len(x)
+        e=identity(dim)
+        arr=empty((dim,dim))
+        for i in range(dim):
+            arr[i][:]=array(((self.grad(x+h*e[:][i])-self.grad(x))/h))
+        return arr
+
 class QN(OPC):
     def __call__(self):
         return False
