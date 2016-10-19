eb57992bef95359221b0ccc768658f8c27f9371f
nlindeke <NiklasLindeke@Niklass-MacBook-Air.local>
2016-10-19 15:42:12 +0200

    test

---
+++ MPIDomainDecomposition/test.py
@@ -0,0 +1,14 @@
+from Heat import Room
+
+a = Room(1)
+b = Room(2,dimy=2)
+c = Room(3)
+
+a.compute_func()
+c.compute_func()
+
+bound1 = a.get_boundary()
+bound2 = c.get_boundary()
+b.boundary(bound2,bound1)
+
+b.compute_func()
