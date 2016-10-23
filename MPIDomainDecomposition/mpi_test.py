543231200f655913342f1d6e6d6bc64f594c05cb
lolpatrol <slanghelvete@gmail.com>
2016-10-23 14:34:42 +0200

    meh

--- MPIDomainDecomposition/mpi_test.py
+++ MPIDomainDecomposition/mpi_test.py
@@ -10,17 +10,18 @@ from scipy import ndimage
 import matplotlib.pyplot as plt
 from mpi4py import MPI
 from numpy import *
-print('hello')
 comm=MPI.COMM_WORLD
 rank=comm.Get_rank()
+#rooms---------------------
 a = Room(1,dx=1.0/3)
 b = Room(2,dimy=2,dx=1.0/3)
 c = Room(3,dx=1.0/3)
+#--------------------------
 leftborder=zeros((a.dimxx,1))
 rightborder=zeros((a.dimxx,1))
 bound1=zeros((a.dimxx,1))
 bound2=zeros((a.dimxx,1))
-nbrIter=1
+nbrIter=10
 omega=0.8
 def plot_func(x,y,z):
     """
@@ -42,14 +43,14 @@ def plot_func(x,y,z):
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.show()
 
-for i in range(nbrIter+1):#+1 to allow for initial boundary setup from large room
+for i in range(nbrIter+1):
     if rank==2:#left room
         comm.Recv(ascontiguousarray(leftborder),source=0)
         temp=a
         temp.border(leftborder)
         temp.compute_func()
         a.compute_func()
-        a.matrice=omega*a.matrice+(1-omega)*temp.matrice #relaxation
+        a.matrice=omega*a.matrice+(1-omega)*temp.matrice
         bound1 = a.get_boundary()
         if i==nbrIter:
             comm.Send(a.matrice,dest=0)
@@ -62,7 +63,7 @@ for i in range(nbrIter+1):#+1 to allow for initial boundary setup from large roo
         temp.border(rightborder)
         temp.compute_func()
         c.compute_func()
-        c.matrice=omega*c.matrice+(1-omega)*temp.matrice #relaxation
+        c.matrice=omega*c.matrice+(1-omega)*temp.matrice
         bound2 = c.get_boundary()
         if i==nbrIter:
             comm.Send(c.matrice,dest=0)
@@ -70,7 +71,6 @@ for i in range(nbrIter+1):#+1 to allow for initial boundary setup from large roo
             comm.Send(ascontiguousarray(bound2),dest=0)
         
     if rank==0:#big room
-        print("hello from BIG, i is: ",i)
         if i==0:
             bound1=transpose(zeros((2,1)))
             bound2=transpose(zeros((2,1)))
@@ -81,14 +81,13 @@ for i in range(nbrIter+1):#+1 to allow for initial boundary setup from large roo
         temp.boundary(bound2,bound1)
         temp.compute_func()
         b.compute_func()
-        b.matrice=omega*b.matrice+(1-omega)*temp.matrice #relaxation
+        b.matrice=omega*b.matrice+(1-omega)*temp.matrice
         boundaries=b.get_boundary()
         leftborder=boundaries[1]
         rightborder=boundaries[0]
         comm.Send(ascontiguousarray(leftborder),dest=2)
         comm.Send(ascontiguousarray(rightborder),dest=1)
         if i == nbrIter:
-            print(b.matrice)
             comm.Recv(a.matrice,source=2)
             comm.Recv(c.matrice,source=1)
             plot_func(a.matrice,b.matrice,c.matrice)
